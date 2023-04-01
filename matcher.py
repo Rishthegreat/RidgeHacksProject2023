98ed51b75a33c1b60c5a9a66ac7a2b8fdccb4c95
LeverActionGT <63264121+LeverActionGT@users.noreply.github.com>
2023-04-01 10:27:17 -0400

    Started matcher.py

---
+++ matcher.py
@@ -0,0 +1,15 @@
+# class Matcher
+
+class Matcher():
+    def __init__(self):
+        pass
+
+    # update all matches
+    # called on timer
+    def update_all_matches(self):
+        pass
+
+    # update match for a single user
+    # used on refresh
+    def search_for_match(self, user_id):
+        pass
